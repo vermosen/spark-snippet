6ae05622f8d7aeb7aadd98760a63300ce63d44af
vermosen <vermosen.jm@free.fr>
2016-09-17 14:35:15 -0400

    added postgresql dependencies

--- src/main/scala/com/app/sparkConnect.scala
+++ src/main/scala/com/app/sparkConnect.scala
@@ -5,10 +5,14 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.log4j.Logger
 
+import java.sql.{Connection, DriverManager, ResultSet};
+import org.postgresql.Driver
+
+import org.apache.spark.sql._
+
 object sparkConnect 
 {
     // connect to a postgresql database
-    // bla
     def main(arg: Array[String]) 
     {
         val conf = new SparkConf()
@@ -17,6 +21,22 @@ object sparkConnect
       
         val sc = new SparkContext(conf)
             sc.setLogLevel("WARN")
+        
+        // Change to Your Database Config
+        classOf[org.postgresql.Driver]
+        val cStr = "jdbc:postgresql://localhost:5432/aletheia?user=posgres&password=1234"
+
+        // Setup the connection
+        val cnx = DriverManager.getConnection(cStr)
+        
+        try
+        {
+            
+        }
+        finally 
+        {
+            cnx.close
+        }
         
         // do stuff
         println("Hello, world!")
