5503f91751aa1d93c9cd52c7895134e1994ba5f0
vermosen <vermosen.jm@free.fr>
2016-09-18 15:58:17 -0400

    db access using new spark session

--- src/main/scala/com/app/sparkConnect.scala
+++ src/main/scala/com/app/sparkConnect.scala
@@ -3,45 +3,75 @@ package com.app
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import org.apache.log4j.Logger
+import org.apache.log4j.{Level, Logger}
 
 import java.sql.{Connection, DriverManager, ResultSet};
 import org.postgresql.Driver
 
 import org.apache.spark.sql._
 
-object sparkConnect 
+object sparkConnect
 {
     // connect to a postgresql database
     def main(arg: Array[String]) 
     {
-        val conf = new SparkConf()
-            conf.setAppName("helloWorld")
-                .setMaster("local")
-      
-        val sc = new SparkContext(conf)
-            sc.setLogLevel("WARN")
-        
-        // Change to Your Database Config
-        classOf[org.postgresql.Driver]
-        val cStr = "jdbc:postgresql://localhost:5432/aletheia?user=posgres&password=1234"
+        val rootLogger = Logger.getRootLogger()
+        rootLogger.setLevel(Level.ERROR)
 
-        // Setup the connection
-        val cnx = DriverManager.getConnection(cStr)
+        val nPart = 10
         
+        // new spark 2.0.0 syntax
+        val spark = SparkSession
+            .builder()
+            .master    ("local[*]")
+            .appName   ("spark.connect")
+            .enableHiveSupport()
+            //.config    ("spark.logLevel", "WARN")
+            .getOrCreate()
+            
+        spark.conf.set("spark.sql.shuffle.partitions", 6)
+        spark.conf.set("spark.executor.memory", "2g")
+        spark.conf.set("spark.driver.memory", "2g")
+                  
         try
         {
+            // database
+            val cnxStr = "jdbc:postgresql://localhost:5432/aletheia"
+            val user   = "postgres"
+            val pwd    = "1234"
             
+            // Setup the connection
+            val cnx = DriverManager.getConnection(cnxStr, user, pwd)
+            
+            try
+            {
+                val df = spark.read
+                    .format("jdbc")
+                    .option("url", cnxStr)
+                    .option("user", user)
+                    .option("password", pwd)
+                    .option("dbtable", "index")
+                    .load()
+                    
+                println(df.head())
+                
+            }
+            catch
+            {
+                case e: Exception => println("an exception occurred: " + e.getMessage)
+            }
+            finally
+            {
+                cnx.close
+            }
         }
-        finally 
+        catch
         {
-            cnx.close
+            case e: Exception => println("an exception occurred: " + e.getMessage)
         }
-        
-        // do stuff
-        println("Hello, world!")
-        
-        // terminate spark context
-        sc.stop()
+        finally
+        {
+            spark.stop                      // terminate spark session
+        }
     }
 }
