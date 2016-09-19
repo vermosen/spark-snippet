bec25e20b85d8fec5b6d2a5776b821127c1249b1
vermosen <vermosen.jm@free.fr>
2016-09-19 07:32:10 -0400

    added sql query

--- src/main/scala/com/app/sparkConnect.scala
+++ src/main/scala/com/app/sparkConnect.scala
@@ -15,13 +15,15 @@ object sparkConnect
     // connect to a postgresql database
     def main(arg: Array[String]) 
     {
-        val rootLogger = Logger.getRootLogger()
-        rootLogger.setLevel(Level.ERROR)
 
-        val nPart = 10
+        // disable logger spam
+        Logger.getLogger("org").setLevel(Level.ERROR)
+        Logger.getLogger("akka").setLevel(Level.ERROR)
+        
+        val partSize: Int = 10
         
         // new spark 2.0.0 syntax
-        val spark = SparkSession
+        val spark: SparkSession = SparkSession
             .builder()
             .master    ("local[*]")
             .appName   ("spark.connect")
@@ -31,28 +33,39 @@ object sparkConnect
             
         spark.conf.set("spark.sql.shuffle.partitions", 6)
         spark.conf.set("spark.executor.memory", "2g")
-        spark.conf.set("spark.driver.memory", "2g")
+        spark.conf.set("spark.driver.memory", "2g")    
                   
         try
         {
             // database
-            val cnxStr = "jdbc:postgresql://localhost:5432/aletheia"
-            val user   = "postgres"
-            val pwd    = "1234"
+            val cnxStr: String = "jdbc:postgresql://localhost:5432/aletheia"
+            val user:   String = "postgres"
+            val pwd:    String = "1234"
             
             // Setup the connection
             val cnx = DriverManager.getConnection(cnxStr, user, pwd)
             
             try
             {
-                val df = spark.read
+                
+                val query: String = 
+                    "(" + 
+                        "SELECT timeseries.index, timeseries.date, timeseries.value " +
+                        "FROM timeseries " +
+                        "JOIN index ON timeseries.index = index.id " +
+                        "WHERE index.source = 2" +
+                    ") AS temp"
+                
+                val df: DataFrame = spark.read
                     .format("jdbc")
                     .option("url", cnxStr)
                     .option("user", user)
                     .option("password", pwd)
-                    .option("dbtable", "index")
+                    .option("dbtable", query)
                     .load()
                     
+                println("retrieved " + df.count + " records")
+                println("sample:")
                 println(df.head())
                 
             }
